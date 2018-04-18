39e8549efb818ce731fa29d06bbc772774a9accb
liujun58love <liujun58love@163.com>
2018-04-18 14:22:20 +0800

    提交信息

---
+++ src/com/liujun/service/UserService.java
@@ -0,0 +1,11 @@
+package com.liujun.service;
+
+import com.liujun.po.User;
+
+/**
+ * Created by Administrator on 2018-04-18.
+ */
+public interface UserService {
+
+    public  void inserUserInfo(User user);
+}
---
+++ src/com/liujun/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.liujun.serviceimpl;
+
+import com.liujun.service.UserService;
+import com.liujun.po.User;
+
+/**
+ * Created by Administrator on 2018-04-18.
+ */
+public class UserServiceImpl implements UserService {
+
+    public  void inserUserInfo(User user) {
+
+
+    }
+}
