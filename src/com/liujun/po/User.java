089a8ca236e608e4b35720bdc3f92a1d2c5f2b7b
liujun58love <liujun58love@163.com>
2018-04-18 14:00:19 +0800

    Initial commit

---
+++ src/com/liujun/po/User.java
@@ -0,0 +1,17 @@
+package com.liujun.po;
+
+/**
+ * Created by Administrator on 2018-04-17.
+ */
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private String adress;
+
+    private Integer age;
+
+    private String idcard;
+}
