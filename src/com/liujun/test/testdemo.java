cd502d14aeae246998b5b3afa83f3711cbff9488
liujun <liujun58love@163.com>
2020-05-23 01:15:29 +0800

    submit update file

---
+++ src/com/liujun/test/testdemo.java
@@ -0,0 +1,12 @@
+package com.liujun.test;
+
+/**
+ * @author liujun
+ * @create 2020-05-23-1:02
+ */
+public class testdemo {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
